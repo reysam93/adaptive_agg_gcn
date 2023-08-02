64c94502e84870e08314b3b523404d53ec0d17eb
reysam93 <samuel.rey.escudero@gmail.com>
2023-08-02 12:15:28 +0200

    organizing code

--- gsp_utils/baselines_models.py
+++ gsp_utils/baselines_models.py
@@ -15,8 +15,27 @@ class NodeClassModel:
         self.loss_fn = loss
         self.S = S
 
+    def get_eval_metrics(self, X, labels):
+
+        self.arch.eval()
+        with torch.no_grad():
+            labels_hat = self.arch(self.S, X)
+            loss = self.loss_fn(labels_hat[self.train_mask], labels[self.train_mask])
+            loss_ev = self.loss_fn(labels_hat[self.val_mask], labels[self.val_mask])
+            loss_test = self.loss_fn(labels_hat[self.test_mask], labels[self.test_mask])
+            
+        losses_train = loss.detach().cpu().item()
+        losses_val = loss_ev.detach().cpu().item()
+        losses_test = loss_test.detach().cpu().item()
+
+        accs_train = self.test(X, self.S, labels, self.train_mask)
+        accs_val = self.test(X, self.S, labels, self.val_mask)
+        accs_test = self.test(X, self.S, labels, self.test_mask)
+
+        return losses_train, losses_val, losses_test, accs_train,accs_val, accs_test
+
     def train(self, X, labels, n_epochs, lr, wd, eval_freq=20, optim=torch.optim.Adam, verb=False):
-        opt = optim(self.arch.parameters(), lr=lr, weight_decay=wd)
+        opt = optim(self.arch.parameters(), lr=lr, weight_decay=wd, patience=10)
 
         losses_train, losses_val, losses_test = [np.zeros(n_epochs) for _ in range(3)]
         accs_train, accs_val, accs_test = [np.zeros(n_epochs) for _ in range(3)]
@@ -29,20 +48,13 @@ class NodeClassModel:
             loss.backward()
             opt.step()
 
-            self.arch.eval()
-            with torch.no_grad():
-                loss_ev = self.loss_fn(labels_hat[self.val_mask], labels[self.val_mask])
-                loss_test = self.loss_fn(labels_hat[self.test_mask], labels[self.test_mask])
-            
-            losses_train[i] = loss.detach().cpu().item()
-            losses_val[i] = loss_ev.detach().cpu().item()
-            losses_test[i] = loss_test.detach().cpu().item()
-            accs_train[i] = self.test(X, self.S, labels, self.train_mask)
-            accs_val[i] = self.test(X, self.S, labels, self.val_mask)
-            accs_test[i] = self.test(X, self.S, labels, self.test_mask)
-
+            losses_train[i], losses_val[i], losses_test[i], \
+                accs_train[i], accs_val[i], accs_test[i] = self.get_eval_metrics(X, labels)
+    
             if (i == 0 or (i+1) % eval_freq == 0) and verb:
                 print(f"Epoch {i+1}/{n_epochs} - Loss Train: {losses_train[i]:.3f} - Acc Train: {accs_train[i]:.3f} - Acc Val: {accs_val[i]:.3f} - Acc Test: {accs_test[i]:.3f}", flush=True)
+            
+            # TODO: EARLY STOPPING
 
         losses = {'train': losses_train, 'val': losses_val, 'test': losses_test}
         accs = {'train': accs_train, 'val': accs_val, 'test': accs_test}
@@ -110,39 +122,15 @@ class GF_NodeClassModel(NodeClassModel):
 
             # Step W
             self.gnn_step(X, labels, opt_W, epochs_W)
-            # opt_W.zero_grad()
-            # labels_hat = self.arch(self.S, X)
-            # loss = self.loss_fn(labels_hat[self.train_mask], labels[self.train_mask])
-            # loss.backward()
-            # opt_W.step()
-            
 
             # Step h
             self.gnn_step(X, labels, opt_h, epochs_h)
-            # opt_h.zero_grad()
-            # labels_hat = self.arch(self.S, X)
-            # loss = self.loss_fn(labels_hat[self.train_mask], labels[self.train_mask])
-            # loss.backward()
-            # opt_h.step()
 
             if clamp:
                 self.arch.clamp_h()
 
-            self.arch.eval()
-            with torch.no_grad():
-
-                labels_hat = self.arch(self.S, X)
-                loss = self.loss_fn(labels_hat[self.train_mask], labels[self.train_mask])
-
-                loss_ev = self.loss_fn(labels_hat[self.val_mask], labels[self.val_mask])
-                loss_test = self.loss_fn(labels_hat[self.test_mask], labels[self.test_mask])
-            
-            losses_train[i] = loss.detach().cpu().item()
-            losses_val[i] = loss_ev.detach().cpu().item()
-            losses_test[i] = loss_test.detach().cpu().item()
-            accs_train[i] = self.test(X, self.S, labels, self.train_mask)
-            accs_val[i] = self.test(X, self.S, labels, self.val_mask)
-            accs_test[i] = self.test(X, self.S, labels, self.test_mask)
+            losses_train[i], losses_val[i], losses_test[i], \
+                accs_train[i], accs_val[i], accs_test[i] = self.get_eval_metrics(X, labels)
 
             if (i == 0 or (i+1) % eval_freq == 0) and verb:
                 print(f"Epoch {i+1}/{n_epochs} - Loss Train: {losses_train[i]:.3f} - Acc Train: {accs_train[i]:.3f} - Acc Val: {accs_val[i]:.3f} - Acc Test: {accs_test[i]:.3f}", flush=True)

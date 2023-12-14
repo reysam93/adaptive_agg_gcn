solving normalization bug

gsp_utils/data.py
@@ -40,8 +40,9 @@ def normalize_gso(S, norm_type):
     if norm_type == 'eigen':
         return S/la.norm(S, 2)
     
-    D_inv = np.diag(1/S.sum(1))
-
+    deg_vec = S.sum(1)
+    D_inv = np.diag(np.where(np.isclose(deg_vec, 0), 0, 1/deg_vec))
+    
     if norm_type == 'right':
         return S @ D_inv
     elif norm_type == 'left':

src/arch.py
@@ -163,7 +163,8 @@ class GFGCN_SpowsLayer(GFGCNLayer):
             H += self.h[k+1] * S_pows[k,:,:]
 
         if norm:
-            d_inv_sqr = torch.sqrt(torch.abs(1/H.sum(1)))
+            deg = H.sum(1)
+            d_inv_sqr = torch.sqrt(torch.abs(torch.where(deg == 0, 0, 1/deg)))
             # Replace diagonal matrix by vectors to scale rows/columns
             H = d_inv_sqr * (H.T * d_inv_sqr).T
 